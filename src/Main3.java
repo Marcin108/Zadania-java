ccc25580cf9d643023c94c51242f4805b99beb75
Marcin Berłowski <m.berlowski@gmail.com>
2019-07-13 13:36:13 +0200

    first commit

---
+++ src/Main3.java
@@ -0,0 +1,14 @@
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        System.out.println("Pierwszy program JAVA");
+
+    }
+
+}//to jest komentarz jednolinijkowy
+/*to jest komentarz
+ wielolinijkowy
+ */
+/** komentarz dokumentacyjny
+ */
---
+++ src/Main5.java
@@ -0,0 +1,11 @@
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        System.out.println("Dzisiaj zaczynam kurs");
+        System.out.println("języka JAVA");
+        System.out.print("w CodersLab");
+//public class zamiast coderslab musi być Main5,dopisałem również ln 5 i 6 linijce aby oddzielić text
+    }
+
+}
